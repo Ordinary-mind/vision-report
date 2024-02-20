3fff3daf11e24072b9afcef1f1013339beef3263
ORDINARYMIND\liang <1633263519@qq.com>
2024-02-21 00:00:45 +0800

    打包输出目录改为docs

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,9 @@ export default defineConfig({
   server: {
     host: '0.0.0.0'
   },
-  base: '/vision-report/',
+  build: {
+    outDir: 'docs'
+  },
   // productionSourceMap: false,
   plugins: [
     vue(),
