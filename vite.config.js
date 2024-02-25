875b720f9846b848ca3790549397021906f9eb1a
ORDINARYMIND\liang <1633263519@qq.com>
2024-02-25 11:37:32 +0800

    ！！！ package.json开发包添加 `vite-plugin-html`，打包添加事件

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,5 +23,12 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
+    createHtmlPlugin({
+      inject:{
+        data: {
+          buildTime: new Date().toLocaleString()
+        }
+      }
+    })
   ],
 })
